409af4f15568f19ca0ebcb7dc43f59c314b1f779
Sakshi Devkar <2001sakshidev@gmail.com>
2023-03-10 12:50:22 +0530

    Basic C Program

---
+++ Control Statements/01_E_Program to check Given Number is Even Or Odd.c
@@ -0,0 +1,19 @@
+//Program To Check Whether Given Number is Even or Odd - Using ternary operator
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0;
+
+    printf("\n Enter the Number = ");
+    scanf("%d",&No);
+
+   (No==0)?printf("\n Neutral Number, \n Please Enter Other Number"):(No%2==0)?printf("Even"):printf("Odd");
+
+    printf("\n Thanks!!!");
+
+    getch();
+    return 0;
+}
