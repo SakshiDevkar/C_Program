7f50ba1311467ebcc00698fd42103716d32f677e
Sakshi Devkar <2001sakshidev@gmail.com>
2023-03-20 21:23:27 +0530

    Basic C Program

---
+++ Control Statements/13 Program to Print Table of 17 - Using While.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 1;
+
+    printf("\n Table of 17 is => \n");
+
+    while(i <= 10)
+    {
+        printf("\n\t %d",i * 17);
+        i++;
+    }
+
+    printf("\n Thanks !!!");
+
+    getch();
+    return 0;
+}
---
+++ Control Statements/13_A_Program to Print Table of 17 - Using For Loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    printf("\n Table of 17 is => \n");
+
+    for( i=1; i <= 10; i++ )
+    {
+        printf("\n\t %d",i * 17);
+    }
+
+    printf("\n Thanks !!!");
+
+    getch();
+    return 0;
+}
