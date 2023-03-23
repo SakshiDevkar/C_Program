34f6ecfce001888caa6542dd9904f97b5671b7d3
Sakshi Devkar <2001sakshidev@gmail.com>
2023-03-23 21:50:58 +0530

    Basic C Program

---
+++ Control Statements/17 Looping .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    while(i < 10)
+    {
+        printf("\n %d Welcome",i+101);
+        i++;
+    }
+
+    printf("\n Thanks !!!");
+
+    getch();
+    return 0;
+}
