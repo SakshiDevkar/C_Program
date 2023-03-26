c586adfcc6ce36a5405b010b1403067872a6c41d
Sakshi Devkar <2001sakshidev@gmail.com>
2023-03-26 12:57:30 +0530

    Pattern Program

---
+++ Pattern Printing/Pattern02 (Hollow).c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0, j = 0, r = 0, c = 0;
+
+    printf("\n Enter Row Value for Pattern :");
+    scanf("%d",&r);
+    printf("\n Enter Column Value for Pattern :");
+    scanf("%d",&c);
+
+    printf("\n ================Pattern=================\n\n");
+
+    for(i = 1; i<= r; i++)
+    {
+        for(j =1; j <= c; j++)
+        {
+            if(i == 1 || j == 1 || i == r || j == c)
+            {
+               printf(" * ");
+            }
+            else
+            {
+                 printf("   ");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\n=========================================\n");
+
+    getch();
+    return 0;
+}
