1a9a65c4f089466fd8eab2ed75dcf1033473f056
Sakshi Devkar <96719849+SakshiDevkar@users.noreply.github.com>
2023-07-21 09:36:05 +0530

    Array Program Assignment

---
+++ Array Assignment/01_Create Array Without initlization & Display All Elements.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num[5] = {};
+
+    printf("\n Enter Value for Element Number 1 = ");
+    scanf("%d", &Num[0]);
+    printf("\n Enter Value for Element Number 2 = ");
+    scanf("%d", &Num[1]);
+    printf("\n Enter Value for Element Number 3 = ");
+    scanf("%d", &Num[2]);
+    printf("\n Enter Value for Element Number 4 = ");
+    scanf("%d", &Num[3]);
+    printf("\n Enter Value for Element Number 5 = ");
+    scanf("%d", &Num[4]);
+
+    printf("\n Value of Element 1 = %d .",Num[0]);
+    printf("\n Value of Element 2 = %d .",Num[1]);
+    printf("\n Value of Element 3 = %d .",Num[2]);
+    printf("\n Value of Element 4 = %d .",Num[3]);
+    printf("\n Value of Element 5 = %d .",Num[4]);
+
+    getch();
+    return 0;
+}
+
