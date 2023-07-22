7ba917b5d8a539ea6d3b04c33104a7dfc0d605b2
Sakshi Devkar <96719849+SakshiDevkar@users.noreply.github.com>
2023-07-22 19:22:22 +0530

    Array Program Assignment

---
+++ Array Assignment/11_Create,Accept,Display Array Using Loop - with macro,Find Manimum and Maximun Element in that array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+#define size 5
+
+int main()
+{
+    int i = 0 , Max = 0, Min = 0,num[size];
+
+    for(i = 0; i < size; i++)
+    {
+        printf("\n Enter %d Element = ",i+1);
+        scanf("%d",&num[i]);
+    }
+
+    for(i = 0; i < size; i++)
+    {
+        if( (i == 0) || (num[i] < Min))
+        {
+            Min = num[i];
+        }
+    }
+
+    for(i = 0; i < size; i++)
+    {
+        if((i == 0) || num[i] > Max)
+        {
+            Max= num[i];
+        }
+    }
+
+    printf("\n Maximum Numbers in array is = %d.",Max);
+    printf("\n Maximum Numbers in array is = %d.",Min);
+
+}
