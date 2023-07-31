a2e04a83dcf5a2b29737717765d65f5096cc0581
Sakshi Devkar <96719849+SakshiDevkar@users.noreply.github.com>
2023-07-31 23:03:28 +0530

    ASCII Value To Character

---
+++ Basic/02_ASCII_Value_To_Character.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Val = 0;
+
+    printf("\n ASCII table = ");
+    scanf("%d",&Val);
+
+    printf("\t\t\n ASCII Value To Character %3d = <%c>",Val,Val);
+
+    getch();
+    return 0;
+}
