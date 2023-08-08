052d37d29aeb12c68005bbff9d11596560b9a18e
Sakshi Devkar <96719849+SakshiDevkar@users.noreply.github.com>
2023-08-08 21:14:46 +0530

    Bitwise Right Shift Implementation

---
+++ Bitwise Program/05 Bitwise Right Shift Implementation.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0, Res = 0;
+
+    printf("\n Enter 1st Number = ");
+    scanf("%d",&No);
+
+    printf("\n=========================================\n");
+
+    Res = No >> 2;
+
+    printf("\n Result => %d >> 2 = %d.",No,Res);
+
+    printf("\n\n=========================================\n");
+
+    getch();
+    return 0;
+
+}
+
