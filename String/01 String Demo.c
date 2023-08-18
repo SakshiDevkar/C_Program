03338a2a468dc9cc94dc1c9bc96d0246794c2b36
Sakshi Devkar <96719849+SakshiDevkar@users.noreply.github.com>
2023-08-18 09:36:45 +0530

    String Program

---
+++ String/01 String Demo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cArr[20] =  {'K', 'A', 'R', 'A', 'D'};
+
+    puts(cArr);
+
+    puts("\n Enter a string : ");
+    gets(cArr);
+
+    puts("\n Given String is => ");
+    puts(cArr);
+
+    getch();
+    return 0;
+}
